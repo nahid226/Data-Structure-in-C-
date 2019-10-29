22f6ded8b2249f9ef8f20ac92e037a0868814d5b
nahid226 <nahid.babu@northsouth.edu>
2019-10-29 21:52:18 +0600

    Pointer Basics

--- main.cpp
+++ main.cpp
@@ -1,41 +1,46 @@
 #include <iostream>
 
 using namespace std;
-
+struct moneyType{
+    int dollar;
+    int cents;
+    moneyType *t;
+};
 int main()
 {
-    int row=3,col=3;
-    int **arr= new int*[row];
+    /*int x,*px,**ppx;
+    px=&x;
+    *px=30;
+    ppx=&px;
+    **ppx=40;
 
-    for (int i = 0; i < row; i++)
-    {
-        arr[i] = new int[col];
-    }
+    cout<<x<<endl;
+    cout<<&x<<endl;
+    cout<<**ppx<<endl;
+    cout<<x<<endl;
+    cout<<&px<<endl;
+    cout<<ppx<<endl;
+    */
+    /*
+    //static declaration
+    moneyType a;
+    a.cents=10;
+    a.dollar=20;
+    cout<<a.cents<<endl;
 
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout<<"Enter a element:"<<endl;
-            cin>>a2d[i][j];
-        }
-    }
-    cout<<"Array is: "<<endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout << a2d[i][j] <<"\t";
-        }
-        cout<<endl;
-    }
-
-    for (int i = 0; i < row; i++)
-        delete[] a2d[i];
-
-    delete[] a2d;
-
-
-    return 0;
+    cout<<a.dollar<<endl;
+    */
+    moneyType *a=new moneyType;
+    moneyType *b=new moneyType;
+    moneyType *c=new moneyType;
+    a->cents=20;
+    b->cents=10;
+    c->dollar=30;
+    a->t=b;
+    b->t=c;
+    cout<<a->t->cents<<endl;
+    cout<<b->t->dollar<<endl;
+    cout<<a->cents<<endl;
+    cout<<b->dollar<<endl;
 
 }
